997017e6f4bd50a60e809e531d12d50cbffd14c1
790210171 <790210171@qq.com>
2016-03-16 20:38:58 +0800

    初始化项目

---
+++ TestGitHub/TestGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGitHub
+//
+//  Created by 汤宝铮 on 16/3/16.
+//  Copyright © 2016年 汤宝铮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestGitHub/TestGitHubTests/TestGitHubTests.m
@@ -0,0 +1,39 @@
+//
+//  TestGitHubTests.m
+//  TestGitHubTests
+//
+//  Created by 汤宝铮 on 16/3/16.
+//  Copyright © 2016年 汤宝铮. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestGitHubTests : XCTestCase
+
+@end
+
+@implementation TestGitHubTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
